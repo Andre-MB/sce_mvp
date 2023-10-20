d4099a7a3b054a086fa41a72c267951a2527f213
Filipe <fedzn11@gmail.com>
2023-10-20 20:40:34 -0300

    Added home.php and style.css, Added images from products

---
+++ app/views/home/home.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Recomex</title>
+</head>
+<body>
+
+    <header>
+    <nav>
+        <a class="button-click" href="home.php">
+        <img src="../../../img/logo_recomex2.png" height=" 40vh" alt="">
+        </a>
+        
+        <div class="list">
+            <ul>
+                <li><a href="">Sobre</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="">Localização</li>
+            </ul>
+
+            <ul>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </div>
+        <button class="button-home-entrar">
+            Entrar
+        </button>
+    </nav>
+    </header>
+    
+    <section id="banner">
+        <div class="banner-test">
+            <img src="../../../img/banner-test.png"  alt="">
+        </div>
+    </section>
+
+    <section id="section-products-home">
+        <div class="products">
+            <a href="">
+                <img src="../../../img/papel.png" alt="">
+            </a>
+            <a href="">
+                <p class="name-product">
+                    Etiqueta código de barras
+                </p>
+            </a>
+        </div>
+        <div class="products">
+            <a href="">
+                <img src="../../../img/impressora.png" alt="">
+            </a>
+                <a href="">
+                    <p class="name-product">
+                    Impressora
+                    </p>
+            </a>
+        </div>
+
+        <div class="products">
+            <a href="">
+                <img src="../../../img/calc.png" alt="">
+            </a>
+                <a href="">
+                    <p class="name-product">
+                    Calculadora De Mesa com Bobina
+                    </p>
+            </a>
+        </div>
+
+        <div class="products">
+            <a href="">
+                <img src="../../../img/bobina.png" alt="">
+            </a>
+                <a href="">
+                    <p class="name-product">
+                    Bobina Branca Apergaminhado
+                    </p>
+            </a>
+        </div>
+
+        <div class="products">
+            <a href="">
+                <img src="../../../img/papel.png" alt="">
+            </a>
+                <a href="">
+                    <p class="name-product">
+                    Etiqueta código de barras
+                    </p>
+            </a>
+        </div>
+    </section>
+
+    
+</body>
+</html>
