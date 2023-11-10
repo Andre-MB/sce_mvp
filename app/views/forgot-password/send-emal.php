7d48e17614f88a022c28a68331d030cf057bfbf7
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2023-11-09 22:39:19 -0300

    correção de merge

--- app/views/forgot-password/send-emal.php
+++ app/views/forgot-password/send-emal.php
@@ -1,16 +1,42 @@
 <?php
-include("../../helpers/conexao.php")
+	include("../../helpers/conexao.php");
 
+
+	$erro = array();
+if(isset($_POST["Recuperar"])){
+
+	$email = $mysqli->escape_string($_POST["email"]);
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $erro[] = "E-mail inválido.";
+  }
+
+  $sql_code = "SELECT * FROM usuarios WHERE email = '$email'";
+	  $result = $mysqli->query($sql_code) or die($mysqli->error);
+    $row_usuario = $result->fetch_assoc();
+    $total = $result->num_rows;
+
+  if ($total == 0){
+    $erro[] = "o e-mail informado não está cadastrado";
+  }
+
+  if(count($erro) == 0 && $total > 0){
+    $codigo_recuperar = strtoupper(substr(md5(time()),0,4));
+    echo"".$codigo_recuperar."";
+  }
+
+
+
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/styles/global.css">
-  <link rel="stylesheet" href="styeles/send-email.css">
-  <title>Document</title>
+  <!-- <link rel="stylesheet" href="styeles/send-email.css"> -->
+  <title>RECOMEX - Recuperar Senha</title>
 </head>
 <body>
 <div class="card_send-email">
@@ -20,14 +46,23 @@ include("../../helpers/conexao.php")
 </div>
 <h1 class="message">Digite o seu e-mail no campo abaixo e lhe
 enviaremos um código</h1>
-</div>
 
 <div class="input">
-<input class="send-email" type="email" placeholder="email">
-<button id="button_enter">Enviar</button>
+
+<form action="" method="POST">
+	<input type="email" name="email" placeholder="Seu email" value="<?php echo $_POST["email"]?>">
+	<input type="submit" value="Recuperar" name="Recuperar">
+</form>
+
+<div class="message_error">
+
+  <?php
+  if(count($erro) > 0){foreach($erro as $msg){echo"<p>$msg</p>";}}
+  ?>
+  </div>
+
 </div>
 
-
-
+</div>
 </body>
 </html>
