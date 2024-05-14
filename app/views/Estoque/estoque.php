4be25fe2c8d633e1383896188c7068576077da1f
Andre-MB <andrezinhogame15@gmail.com>
2024-05-13 22:20:52 -0300

    comented animate

--- app/views/Estoque/estoque.php
+++ app/views/Estoque/estoque.php
@@ -35,7 +35,7 @@ $quer = mysqli_query($mysqli, $sql_clientes);
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="venda.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> -->
     <script src="/js/wow.min.js"></script>
     <script>
         new WOW().init();
